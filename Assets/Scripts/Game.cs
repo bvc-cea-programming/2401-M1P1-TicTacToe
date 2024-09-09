4ac9b4cbb6d4f71029a9b39a3ffa55ee7fd35cd8
ECoutinho <e.ferreiracoutinh812@mybvc.ca>
2024-09-09 17:49:02 -0600

    Solved Game
    Followed comments and completed the game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 
 public enum Player
 {
@@ -15,10 +16,10 @@ public enum GameState
 }
 
 // convert this class into an abstract class
-public class Game
+public abstract class Game
 {
     // Why do we use protected here?
-    protected Player currentPlayer;
+    protected Player currentPlayer; //to specify it is used only by the set type of info?
     protected GameState gameState;
 
     // Public properties for external access
@@ -26,19 +27,27 @@ public class Game
     public GameState CurrentState => gameState;
 
     // Create a constructor to initialize the game
-    
+    public Game()
+    {
+        InitializeGame();
+    }
 
     // Create the following abstract methods for the following
     // 1. InitializeGame
+    public abstract void InitializeGame();
     // 2. MakeMove - returns true if the move was successful, parameters should be the row and column of the move
+    public abstract bool MakeMove(int row, int col);
     // 3. CheckWinCondition - returns true if the win condition is met, parameters should be the row and column of the move
+    public abstract bool CheckWinCondition(int row, int col);
     // 4. CheckDrawCondition - returns true if the draw condition is met
+    public abstract bool CheckDrawCondition();
     // 5. GetGameResult - returns the result of the game
-    
+    public abstract string GetGameResult(); 
 
-    // What does this method do?
+
+    // What does this method do? 
     protected void SwitchPlayer()
     {
-        currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
+        currentPlayer = currentPlayer == Player.X ? Player.O : Player.X; //changes turn between players 
     }
 }
