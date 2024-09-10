b1834614ab9edcf2eb5ee23210c257fd82e8f139
Nibour16 <ruobinh3@hotmail.com>
2024-09-09 18:27:40 -0600

    Finish Lab 1

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -15,9 +15,12 @@ public enum GameState
 }
 
 // convert this class into an abstract class
-public class Game
+public abstract class Game
 {
-    // Why do we use protected here?    A: protected variables can be only used for the classes that are internal to the owner class
+    // Why do we use protected here?
+    // A: Protected variables can be only used for the classes that are internal to the owner class,
+    //    since tic tac toe game requires multiple players,
+    //    we need to make the player and the state variables to be accessible from internal classes which are all of the different player classes
     protected Player currentPlayer;
     protected GameState gameState;
 
@@ -26,19 +29,29 @@ public class Game
     public GameState CurrentState => gameState;
 
     // Create a constructor to initialize the game
-    
-
+    public Game() 
+    {
+        InitializeGame();
+    }
     // Create the following abstract methods for the following
     // 1. InitializeGame
     // 2. MakeMove - returns true if the move was successful, parameters should be the row and column of the move
     // 3. CheckWinCondition - returns true if the win condition is met, parameters should be the row and column of the move
     // 4. CheckDrawCondition - returns true if the draw condition is met
     // 5. GetGameResult - returns the result of the game
-    
+
+    public abstract void InitializeGame();
+    public abstract bool MakeMove(int row, int col);
+    protected abstract bool CheckWinCondition(int row, int col);
+    protected abstract bool CheckDrawCondition();
+    public abstract string GetGameResult();
 
     // What does this method do?
     protected void SwitchPlayer()
     {
         currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
+        // A: This method is used to switch the player to another if the player is done the move.
+        //    this method will work to be this way:
+        //    if currentPlayer is equal to X, set player to O; else if current player is equal to O, set player to X.
     }
 }
